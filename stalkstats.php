317c5d8c7b987f8904729eb2c1f0d9564d18c8b5
Diego Escalante Urrelo <diegoe@gnome.org>
2013-06-08 06:14:21 -0500

    stalkstats: add datetime reporting
    Also reorder the script a bit.

--- stalkstats.php
+++ stalkstats.php
@@ -41,9 +41,10 @@ function get_plusones($url) {
     return intval($json[0]['result']['metadata']['globalCounts']['count']);
 }
 
-if ($argc > 1)
+if ($argc > 1) {
+    $no_db = true;
     $url = $argv[1];
-else
+} else
     $url = "http://thecreativechallenge.org/pdetail/unlock-the-ad-free-web/";
 
 $facebook = get_likes($url);
@@ -52,35 +53,48 @@ $google = get_plusones($url);
 $linkedin = get_linkedin($url);
 
 $total = $facebook + $twitter + $google + $linkedin;
+$row = null;
 
-echo date("r") . "\n";
-echo "-------\n";
-echo "Facebook: " . $facebook . "\n";
-echo "Twitter: " . $twitter . "\n";
-echo "Google+: " . $google . "\n";
-echo "LinkedIn: " . $linkedin . "\n";
-echo "-------\n";
-echo "Total: " . $total . "\n";
+if (!$no_db) {
+    if (!file_exists ('db.db'))
+        $new_db = true;
+    else
+        $new_db = false;
 
-if (!file_exists ('db.db'))
-    $new_db = true;
-else
-    $new_db = false;
+    $db = new SQLite3('db.db');
+    if ($new_db) {
+        $sql_create = 'CREATE TABLE stats (datetime INTEGER, facebook INTEGER, twitter INTEGER, googleplus INTEGER, linkedin INTEGER, total INTEGER)';
+        $db->exec($sql_create);
+    }
 
-$db = new SQLite3('db.db');
-if ($new_db) {
-    $sql_create = 'CREATE TABLE stats (datetime INTEGER, facebook INTEGER, twitter INTEGER, googleplus INTEGER, linkedin INTEGER, total INTEGER)';
-    $db->exec($sql_create);
-}
+    $date = time();
 
-$date = time();
+    $sql_time = "SELECT * FROM stats ORDER BY datetime DESC LIMIT 1";
+    $row = $db->query($sql_time)->fetchArray();
 
-$sql_time = "SELECT datetime FROM stats ORDER BY datetime DESC LIMIT 1";
-$row = $db->query($sql_time)->fetchArray();
-
-if (!$row || ($date - $row['datetime']) > 3600) {
-    $sql = "INSERT INTO stats VALUES (${date}, ${facebook}, ${twitter}, ${google}, ${linkedin}, ${total})";
-    $db->exec($sql);
+    if (!$row || ($date - $row['datetime']) > 3600) {
+        $sql = "INSERT INTO stats VALUES (${date}, ${facebook}, ${twitter}, ${google}, ${linkedin}, ${total})";
+        $db->exec($sql);
+    } else {
+        echo "No SQL record created: ${date} - ${row['datetime']} = ". ($date - $row['datetime']) .".\n";
+        echo "-------\n";
+    }
 } else {
-    echo "No SQL record created: ${date} - ${row['datetime']} = ". ($date - $row['datetime']) .".\n";
+
 }
+
+$d_facebook = '(+' . ($facebook - $row['facebook']) . ')';
+$d_twitter = '(+' . ($twitter - $row['twitter']) . ')';
+$d_google = '(+' . ($google - $row['googleplus']) . ')';
+$d_linkedin = '(+' . ($linkedin - $row['linkedin']) . ')';
+
+
+echo "NOW:  " . date('r') . "\n";
+echo "LAST: " . date('r', $row['datetime']) . "\n";
+echo "-------\n";
+echo "Facebook: " . $facebook . $d_facebook . "\n";
+echo "Twitter: " . $twitter . $d_twitter . "\n";
+echo "Google+: " . $google . $d_google . "\n";
+echo "LinkedIn: " . $linkedin . $d_linkedin . "\n";
+echo "-------\n";
+echo "Total: " . $total . $d_total . "\n";
